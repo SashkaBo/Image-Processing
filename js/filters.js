ea9a8143b18b7083a429ea32decd00e4e880a737
idej <idej.wrk@gmail.com>
2014-03-11 15:29:19 +0300

    add CanvasImage class

--- js/filters.js
+++ js/filters.js
@@ -1,38 +1,29 @@
-function simpleBlur(){
-    alert(1);
+function CanvasImage(canvas, src) {
+  var context = canvas.getContext('2d');
+  var i = new Image();
+  var that = this;
+  image.onload = function() {
+    canvas.width = i.width;
+    canvas.height = i.height;
+    context.drawImage(i, 0, 0, i.width, i.height);
+    that.original = that.getData();
+  };
+  i.src = src;
+  this.context = context;
+  this.image = i;
+}
 
+var transformador = new CanvasImage($('pic'), $('#my-image').attr('src'))
+
+CanvasImage.prototype.getData = function() {
+  return this.context.getImageData(0, 0, this.image.width, this.image.heiht);
 };
 
-Filters = {};
-
-Filters.getPixels = function(img) {
-    var c = this.getCanvas(img.width, img.height);
-    var ctx = c.getContext('2d');
-    ctx.drawImage(img);
-    return ctx.getImageData(0,0,c.width,c.height);
+CanvasImage.prototype.setData = function(data) {
+  return this.context.putImageData(data, 0, 0);
 };
 
-Filters.getCanvas = function(w,h) {
-    var c = document.createElement('canvas');
-    c.width = w;
-    c.height = h;
-    return c;
+CanvasImage.prototype.reset = function() {
+  this.setData(this.original);
 };
 
-Filters.filterImage = function(filter, image, var_args) {
-    var args = [this.getPixels(image)];
-    for (var i=2; i<arguments.length; i++) {
-        args.push(arguments[i]);
-    }
-    return filter.apply(null, args);
-};
-
-Filters.brightness = function(pixels, adjustment) {
-    var d = pixels.data;
-    for (var i=0; i<d.length; i+=4) {
-        d[i] += adjustment;
-        d[i+1] += adjustment;
-        d[i+2] += adjustment;
-    }
-    return pixels;
-};
